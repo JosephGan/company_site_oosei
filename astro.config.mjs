9cd8a79883e04aa775a377276af9b113f8eaf999
josephgan <ganjianmail@gmail.com>
2025-12-05 14:47:22 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com', // 以后部署到 GitHub Pages 时再改
+  site: 'https://oosei.net', // 以后部署到 GitHub Pages 时再改
+  base: '/',
   output: 'static', 
 });
